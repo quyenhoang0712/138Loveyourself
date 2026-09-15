eae4de0a4aa656e3428a04e6dda9f42c5d0690cc
quyenhoang0712 <quyenhoang0712@users.noreply.github.com>
2026-09-16 03:33:02 +0700

    update

--- api/community-memories/[...path].js
+++
@@ -1,9 +0,0 @@
-import app from '../../backend/app.js'
-
-export default function handler(req, res) {
-  if (!req.url.startsWith('/api/community-memories')) {
-    req.url = `/api/community-memories${req.url.startsWith('/') ? req.url : `/${req.url}`}`
-  }
-
-  return app(req, res)
-}
--- api/content-messages/[...path].js
+++
@@ -1,9 +0,0 @@
-import app from '../../backend/app.js'
-
-export default function handler(req, res) {
-  if (!req.url.startsWith('/api/content-messages')) {
-    req.url = `/api/content-messages${req.url.startsWith('/') ? req.url : `/${req.url}`}`
-  }
-
-  return app(req, res)
-}
